0072ef08b09700192d59f03accfbfa445b4ccadb
Léo Figea <59359380+malkovro@users.noreply.github.com>
2021-12-03 17:24:37 +0100

    Day 3 - Binary Diagnostic (part 2)

--- 03_binary_diagnostic/src/main.rs
+++ 03_binary_diagnostic/src/main.rs
@@ -11,35 +11,19 @@ fn main() {
         .map(|x| u32::from_str_radix(x, 2).unwrap())
         .collect();
     let size = lines.first().unwrap().len();
-    let base: usize = 2;
-    let mut mask_counts = vec![0; size];
-    for (bit, _e) in mask_counts.clone().iter().enumerate() {
-        let mask = base.pow(bit.clone().try_into().unwrap()) as u32;
-        for diagnostic_number in diagnostic_numbers.clone() {
-            if diagnostic_number & mask > 0 {
-                mask_counts[bit] += 1;
-            }
-        }
-    }
-
-    println!("{:?}", mask_counts);
-    let threshold = &(lines.len() / 2);
-    let gamma_binary = mask_counts
-        .iter()
-        .rev()
-        .map(|count| {
-            if count > threshold {
-                "1".to_string()
-            } else {
-                "0".to_string()
-            }
-        })
-        .collect::<Vec<String>>()
-        .join("");
-    let gamma = u32::from_str_radix(&gamma_binary, 2).unwrap();
-
-    let epsilon: u32 = (2 as u32).pow(size.try_into().unwrap()) as u32 - gamma - 1;
-    println!("{:?} * {:?}: {:?}", gamma, epsilon, gamma * epsilon);
+    let gamma = find_gamma_rating(diagnostic_numbers.clone(), size);
+    let epsilon = (2 as u32).pow(size as u32) - 1 - gamma;
+    let oxy_rating = find_oxygen_generator_rating(diagnostic_numbers.clone(), size);
+    let co2_rating = find_co2_scrubber_rating(diagnostic_numbers, size);
+    println!(
+        "gamma({:?}) * epsilon({:?}): power({:?})\noxygen_rating({:?}) * co2_rating({:?}): life_support_rating({:?})",
+        gamma,
+        epsilon,
+        gamma * epsilon,
+        oxy_rating,
+        co2_rating,
+        oxy_rating * co2_rating
+    );
 }
 
 fn read_into_vec(file_name: &str) -> Vec<String> {
@@ -50,4 +34,85 @@ fn read_into_vec(file_name: &str) -> Vec<String> {
         .filter(|s| !s.is_empty())
         .map(String::from)
         .collect()
+}
+
+fn find_gamma_rating(diagnostic_numbers: Vec<u32>, size: usize) -> u32 {
+    let base: usize = 2;
+    u32::from_str_radix(
+        &(0..size)
+            .rev()
+            .fold("".to_string(), |binary_rep: String, bit: usize| {
+                let mask = base.pow(bit.clone().try_into().unwrap()) as u32;
+                let high_levels_count = diagnostic_numbers
+                    .iter()
+                    .filter(|number| *number & mask > 0)
+                    .count();
+                let bit_value = if high_levels_count * 2 >= diagnostic_numbers.len() {
+                    "1"
+                } else {
+                    "0"
+                };
+                binary_rep + bit_value
+            }),
+        2,
+    )
+    .unwrap()
+}
+
+fn find_oxygen_generator_rating(mut diagnostic_numbers: Vec<u32>, size: usize) -> u32 {
+    let base: usize = 2;
+    for bit in (0..size).rev() {
+        let mask = base.pow(bit.clone().try_into().unwrap()) as u32;
+        let candidates_count = diagnostic_numbers.len();
+        let high_levels_count = diagnostic_numbers
+            .iter()
+            .filter(|number| *number & mask > 0)
+            .count();
+        if high_levels_count * 2 >= candidates_count {
+            diagnostic_numbers = diagnostic_numbers
+                .iter()
+                .filter(|number| *number & mask > 0)
+                .map(|x| *x)
+                .collect();
+        } else {
+            diagnostic_numbers = diagnostic_numbers
+                .iter()
+                .filter(|number| *number & mask == 0)
+                .map(|x| *x)
+                .collect();
+        }
+        if diagnostic_numbers.len() == 1 {
+            return diagnostic_numbers[0];
+        }
+    }
+    panic!("No diagnostic_number seemed to match!");
+}
+
+fn find_co2_scrubber_rating(mut diagnostic_numbers: Vec<u32>, size: usize) -> u32 {
+    let base: usize = 2;
+    for bit in (0..size).rev() {
+        let mask = base.pow(bit.clone().try_into().unwrap()) as u32;
+        let candidates_count = diagnostic_numbers.len();
+        let high_levels_count = diagnostic_numbers
+            .iter()
+            .filter(|number| *number & mask > 0)
+            .count();
+        if high_levels_count * 2 >= candidates_count {
+            diagnostic_numbers = diagnostic_numbers
+                .iter()
+                .filter(|number| *number & mask == 0)
+                .map(|x| *x)
+                .collect();
+        } else {
+            diagnostic_numbers = diagnostic_numbers
+                .iter()
+                .filter(|number| *number & mask > 0)
+                .map(|x| *x)
+                .collect();
+        }
+        if diagnostic_numbers.len() == 1 {
+            return diagnostic_numbers[0];
+        }
+    }
+    panic!("No diagnostic_number seemed to match!");
 }
